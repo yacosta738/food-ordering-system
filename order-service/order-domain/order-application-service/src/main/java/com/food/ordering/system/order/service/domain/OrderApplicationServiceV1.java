dfec34eef0ed079054de1624388c38c64cfc3e53
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2022-07-11 21:40:13 -0300

    moved OrderApplicationServiceV1 🦍

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApplicationServiceV1.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApplicationServiceV1.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.domain.ports;
+package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.OrderCreateCommandHandler;
 import com.food.ordering.system.order.service.domain.OrderTrackCommandHandler;
